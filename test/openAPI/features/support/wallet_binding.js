963a486798154a06d91ebe921c24e7ee13437314
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-07-20 11:55:52 +0200

    TECH-732: Added new test case for invalid challenge, fix the test for duplicated public key, added test step for check the expireDateTime

--- test/openAPI/features/support/wallet_binding.js
+++ test/openAPI/features/support/wallet_binding.js
@@ -12,13 +12,22 @@ const {
 chai.use(require('chai-json-schema'));
 
 let specWalletBinding;
+let specWalletBindingDuplicate;
 let providedAuthFactorType;
-const publicKey = JSON.stringify({
+const publicKeyOne = JSON.stringify({
   kty: 'RSA',
   a: 'AQAB',
   use: 'sig',
   alg: 'RS256',
   n: 'mykWIftknK1TQmbiazuik0rWGsxeOIUE3yfSQJgoCfdGXY4HfHE6AlNKFdIKZOXe-U-L21Klj692e9iZx05rHHaZvO0a4IzyFMOyw5wjBCWoBOcA4q93LPkZTSkIq9I2Vgr6Bzwu6X7QPMbmF8xAKX4KeSn_yZcsAhElHBOWkENmKp76yCyTeE4DAIGah1BcgiB_KWvOZOedwTRDLyQ0DZM1z07-N-rPh0qSd2UFRRY-b_jc9opjyRQq3d5ZkiB9W4ReAUhIKA9uc1RDs1shc3G8zgZp3qH6fYWmsOi23BOA_q8Z-wMHwPK2vEJvgZIWovAG5jGFbMilNcFQfzLJcQ',
+});
+
+const publicKeyTwo = JSON.stringify({
+  kty: 'RSA',
+  a: 'AQAB',
+  use: 'sig',
+  alg: 'RS256',
+  n: 'VFUiPhpONKm54WyhTgQJ1ONENwX1X6nR4qyNWAODKFXsYG20EZtJwLyuuL35D1oBmLHhQY9zdWVZZVy5r8uJ3ueb0pfl8ZqO5BNvgeoDM6dextpI6sntQCUcRuERB7oC9FrrWPYkKO9lFQfrJFRhmDtN4XviEpJAXW5Lpx0iNXJnXNrFZOcKrfQfAWPxBrIFPMJRBy6giM1RJAjwjHQ8ULOWGRpoIOGyQy2IOWKG6f0UrFaO9qUImBR82aPumSAuJaslRFThM4n6AJFrrhBT5nAgS6jPf9D48NYkXRroJnTlJqm7kVgfOMjPKgQ7T9JsJ9MTpV5keTQSwE2GHZNa1P',
 });
 
 const base64ToJson = (publicKey) => {
@@ -31,6 +40,7 @@ const endpointTag = { tags: `@endpoint=/${walletBindingEndpoint}` };
 
 Before(endpointTag, () => {
   specWalletBinding = spec();
+  specWalletBindingDuplicate = spec();
 });
 
 // Scenario: Successfully validates the wallet and generates the wallet user id smoke type test
@@ -48,12 +58,14 @@ When(
               individualId: individualId,
               authFactorType: authFactorType,
               format: format,
-              challengeList: {
-                authFactorType: authFactorType,
-                challenge: challenge,
-                format: format,
-              },
-              publicKey: base64ToJson(publicKey),
+              challengeList: [
+                {
+                  authFactorType: authFactorType,
+                  challenge: challenge,
+                  format: format,
+                }
+              ],
+              publicKey: base64ToJson(publicKeyOne),
             },
           })
 
@@ -103,9 +115,13 @@ Then(
 );
 
 Then(
-    /^The \/wallet\-binding endpoint response should contain a future expireDateTime$/,
-    () =>
-        chai.expect(specWalletBinding._response.json.response.expireDateTime).to.not.be.empty
+    /^The \/wallet\-binding endpoint response should contain a future expireDateTime in ISO 8601 format$/,
+    () => {
+      const expireDateTime = specWalletBinding._response.json.response.expireDateTime;
+      const iso8601Regex = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i;
+
+      chai.expect(expireDateTime > new Date().toISOString() && iso8601Regex.test(expireDateTime)).to.be.true;
+    }
 );
 
 // Scenario: Not able to generate the wallet binding because of unsupported challenge format
@@ -121,12 +137,14 @@ When(
                 individualId: individualId,
                 authFactorType: authFactorType,
                 format: format,
-                challengeList: {
-                  authFactorType: authFactorType,
-                  challenge: challenge,
-                  format: format,
-                },
-                publicKey: base64ToJson(publicKey),
+                challengeList: [
+                  {
+                    authFactorType: authFactorType,
+                    challenge: challenge,
+                    format: format,
+                  }
+                ],
+                publicKey: base64ToJson(publicKeyOne),
               },
             })
 );
@@ -166,17 +184,20 @@ When(
           individualId: individualId,
           authFactorType: authFactorType,
           format: format,
-          challengeList: {
-            authFactorType: authFactorType,
-            challenge: challenge,
-            format: format,
-          },
-          publicKey: publicKey,
+          challengeList: [
+            {
+              authFactorType: authFactorType,
+              challenge: challenge,
+              format: format,
+            }
+          ],
+          publicKey: publicKeyOne,
         },
       })
 );
 
 // Scenario: Not able to generate the wallet binding because of duplicated public key
+// Given and others Then for this scenario are written in the aforementioned example
 When(
   /^Send POST \/wallet\-binding request with given "([^"]*)" as individualId and "([^"]*)" as authFactorType and "([^"]*)" as format and "([^"]*)" as challenge and duplicated publicKey$/,
   (individualId, authFactorType, format, challenge) => {
@@ -188,18 +209,45 @@ When(
             individualId: individualId,
             authFactorType: authFactorType,
             format: format,
-            challengeList: {
-              authFactorType: authFactorType,
-              challenge: challenge,
-              format: format,
-            },
-            publicKey: base64ToJson(publicKey),
+            challengeList: [
+              {
+                authFactorType: authFactorType,
+                challenge: challenge,
+                format: format,
+              }
+            ],
+            publicKey: base64ToJson(publicKeyTwo),
           },
         })
 
     providedAuthFactorType = authFactorType;
   });
 
+When (/^Send POST \/wallet-binding request with the same public key as in the previous request$/, () => {
+  specWalletBindingDuplicate
+      .post(baseUrl)
+      .withJson({
+        requestTime: new Date().toISOString(),
+        request: {
+          individualId: 'n3fy2qkg9r7h2',
+            authFactorType: 'OTP',
+            format: 'encoded-json',
+            challengeList: [
+              {
+                authFactorType: 'OTP',
+                challenge: 'string',
+                format: 'encoded-json',
+              }
+            ],
+          publicKey: base64ToJson(publicKeyTwo),
+        },
+      })
+})
+
+// Scenario: Not able to generate the wallet binding because of invalid auth challenge
+// Given and others Then for this scenario are written in the aforementioned example
+
 After(endpointTag, () => {
   specWalletBinding.end();
+  specWalletBindingDuplicate.end();
 });
--- test/openAPI/features/wallet_binding.feature
+++ test/openAPI/features/wallet_binding.feature
@@ -11,7 +11,7 @@ Feature: The endpoint to validate wallet create wallet user id
     And The /wallet-binding endpoint endpoint response should have content-type: application/json header
     And The /wallet-binding endpoint response should match json schema with no errors
     And The /wallet-binding endpoint response should have authFactorType value should be equal to specified enum
-    And The /wallet-binding endpoint response should contain a future expireDateTime 
+    And The /wallet-binding endpoint response should contain a future expireDateTime in ISO 8601 format
 
   @negative
   Scenario: Not able to generate the wallet binding because of unsupported challenge format
@@ -40,8 +40,21 @@ Feature: The endpoint to validate wallet create wallet user id
     Given Wants to validate the wallet and generate wallet user id
     When Send POST /wallet-binding request with given "n3fy2qkg9r7h2" as individualId and "OTP" as authFactorType and "encoded-json" as format and "string" as challenge and duplicated publicKey
     Then Receive a response from the /wallet-binding endpoint
+    When Send POST /wallet-binding request with the same public key as in the previous request
+    Then Receive a response from the /wallet-binding endpoint
     And The /wallet-binding response should be returned in a timely manner 15000ms
     And The /wallet-binding endpoint response should have status 200
     And The /wallet-binding endpoint endpoint response should have content-type: application/json header
     And The /wallet-binding endpoint response should match json schema with errors
     And The /wallet-binding response should contain errorCode property equals to "duplicate_public_key"
+
+  @negative
+  Scenario: Not able to generate the wallet binding because of invalid auth challenge
+    Given Wants to validate the wallet and generate wallet user id
+    When Send POST /wallet-binding request with given "n3fy2qkg9r7h2" as individualId and "OTP" as authFactorType and "encoded-json" as format and "" as challenge and publicKey
+    Then Receive a response from the /wallet-binding endpoint
+    And The /wallet-binding response should be returned in a timely manner 15000ms
+    And The /wallet-binding endpoint response should have status 200
+    And The /wallet-binding endpoint endpoint response should have content-type: application/json header
+    And The /wallet-binding endpoint response should match json schema with errors
+    And The /wallet-binding response should contain errorCode property equals to "invalid_auth_challenge"
